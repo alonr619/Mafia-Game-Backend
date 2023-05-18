393c3db90a5546202d66142c37d9d6033da8e6a5
RAGOLER <s-aragoler@lwsd.org>
2023-05-18 14:20:29 -0700

    lint

--- supabase/functions/createNewGame.ts
+++ supabase/functions/createNewGame.ts
@@ -1,12 +1,16 @@
-import { serve } from 'https://deno.land/std@0.188.0/http/server.ts';
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import {
+    serve
+} from 'https://deno.land/std@0.188.0/http/server.ts';
+import {
+    createClient
+} from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabaseClientAdmin = createClient(
     Deno.env.get('SUPABASE_URL') ?? '',
     Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
 )
 
-async function makeKey(length: number): Promise<string> {
+async function makeKey(length: number): Promise < string > {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
@@ -18,22 +22,41 @@ async function makeKey(length: number): Promise<string> {
     return result;
 }
 
+/**
+ * Request body has all the config variables needed. Sent from already authorized person
+ */
 serve(async (req: Request) => {
-  try {
-    const supabaseClientUser = createClient(
-      Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
-    )
-    const {
-      data: { user },
-    } = await supabaseClientUser.auth.getUser();
+    try {
+        const supabaseClientUser = createClient(
+            Deno.env.get('SUPABASE_URL') ?? '',
+            Deno.env.get('SUPABASE_ANON_KEY') ?? '', {
+                global: {
+                    headers: {
+                        Authorization: req.headers.get('Authorization') !
+                    }
+                }
+            }
+        )
+        const {
+            data: {
+                user
+            },
+        } = await supabaseClientUser.auth.getUser();
 
-    const { max_players, num_mafia, num_doctor, num_detective, num_vigilante, is_public } = await req.json();
-    const key = await makeKey(8);
+        // Config variables
+        const {
+            max_players,
+            num_mafia,
+            num_doctor,
+            num_detective,
+            num_vigilante,
+            is_public
+        } = await req.json();
+        const key = await makeKey(8);
 
-    const { data, error } = supabaseClientAdmin.from('upcoming_games').insert([
-        {
+        const {
+            error
+        } = supabaseClientAdmin.from('upcoming_games').insert([{
             id: key,
             host: user.id,
             lobby: [],
@@ -44,18 +67,25 @@ serve(async (req: Request) => {
             num_detective,
             num_vigilante,
             is_public
-        }
-    ]);
-    if (error) throw error;
+        }]);
+        if (error) throw error;
 
-    return new Response(JSON.stringify({ key }), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 200,
-    })
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 400,
-    })
-  }
+        return new Response(JSON.stringify({
+            key
+        }), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            status: 200,
+        })
+    } catch (error) {
+        return new Response(JSON.stringify({
+            error: error.message
+        }), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            status: 400,
+        })
+    }
 })
